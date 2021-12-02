46c0ad44d0bc8cafb3c40a07aa9bdbe15c4b2371
KamilKurde <kamilbak113@gmail.com>
2021-12-02 09:27:08 +0100

    Finished day 1 challenge

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,22 @@
+fun List<String>.toIntList() = this.filterNot { it.isEmpty() }.map { it.toInt() }
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+	fun part1(input: List<String>): Int {
+		val numbers = input.toIntList()
+		var counter = 0
+		for (i in 1 until numbers.size)
+			if (numbers[i] > numbers[i - 1])
+				counter++
+		return counter
+	}
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+	fun part2(input: List<String>): Int {
+		val numbers = input.toIntList()
+		val windowed = numbers.windowed(3, partialWindows = true)
+		return part1(windowed.map { it.sum().toString() })
+	}
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+	val input = readInput("Day01")
+	println(part1(input))
+	println(part2(input))
 }
