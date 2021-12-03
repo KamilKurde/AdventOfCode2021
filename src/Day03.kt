da9b5c40e72767b85cc0b2ebef8096b434d523e5
KamilKurde <kamilbak113@gmail.com>
2021-12-03 09:56:12 +0100

    Completed day 3 part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -14,7 +14,29 @@ fun main() {
 	}
 
 	fun part2(input: List<String>): Int {
-		TODO()
+		fun getNumber(char: Char): String {
+			var bit = 0
+			val unwantedChar = if (char == '1') '0' else '1'
+			var data = input.filterNot { it.isEmpty() }.groupBy { it[0] }
+			while (data.values.flatten().size != 1) {
+				val wanted = data[char]?.size ?: 0
+				val unwanted = data[unwantedChar]?.size ?: 0
+				val mostCommonChar: Char = if (
+					when (char) {
+						'0' -> wanted > unwanted
+						'1' -> wanted >= unwanted
+						else -> throw IllegalArgumentException("Unknown char: $char")
+					}
+				) '1' else '0'
+				val filteredData = data.values.flatten().filter { it[bit] == mostCommonChar }
+				if (filteredData.size == 1)
+					return filteredData.first()
+				bit++
+				data = filteredData.groupBy { it[bit] }
+			}
+			throw IllegalArgumentException("Input data is insufficient")
+		}
+		return getNumber('1').toInt(2) * getNumber('0').toInt(2)
 	}
 
 	val input = readInput("Day03")
