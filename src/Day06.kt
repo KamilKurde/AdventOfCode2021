8310365d9a6a159892b00601d03f5b2140102b05
KamilKurde <kamilbak113@gmail.com>
2021-12-10 12:00:23 +0100

    Finished day 6 part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,32 +1,28 @@
-import java.util.*
-
-class LanternFish(val creatingInterval: Int, var remainingDays: Int = creatingInterval) {
-	fun nextDay(list: MutableList<LanternFish>) {
-		remainingDays--
-		if (remainingDays < 0) {
-			remainingDays = creatingInterval
-			list += LanternFish(creatingInterval, creatingInterval + 2)
-		}
-	}
-}
-
 fun main() {
-	fun howManyLanternFishes(days: Int, initialState: MutableList<LanternFish>): Int {
-		val lanternFishes = if(days > 100) LinkedList<LanternFish>() else mutableListOf()
-		lanternFishes += initialState
+	fun howManyLanternFishes(days: Int, initialList: List<String>): ULong {
+		var lanternFishesIndex = Array(9) { 0UL }
+		initialList.map { it.toInt() }.forEach {
+			lanternFishesIndex[it]++
+		}
 		repeat(days)
 		{
-			for (i in lanternFishes.indices)
-				lanternFishes[i].nextDay(lanternFishes)
-			println("$it: ${lanternFishes.size}")
-			//println("After ${it + 1} days: ${lanternFishes.map { it.remainingDays }.joinToString(",")}")
+			val after = Array(9) { 0UL }
+			for (i in lanternFishesIndex.indices) {
+				if (i == 0) {
+					after[8] = lanternFishesIndex[i]
+					after[6] = lanternFishesIndex[i]
+				} else {
+					after[i - 1] += lanternFishesIndex[i]
+				}
+			}
+			lanternFishesIndex = after
 		}
-		return lanternFishes.size
+		return lanternFishesIndex.sum()
 	}
 
-	fun part1(input: List<String>) = howManyLanternFishes(80, input.joinToString(",").split(",").map { LanternFish(6, it.toInt()) }.toMutableList())
+	fun part1(input: List<String>) = howManyLanternFishes(80, input.joinToString(",").split(","))
 
-	fun part2(input: List<String>) = howManyLanternFishes(256, input.joinToString(",").split(",").map { LanternFish(6, it.toInt()) }.toMutableList())
+	fun part2(input: List<String>) = howManyLanternFishes(256, input.joinToString(",").split(","))
 
 	val input = readInput("Day06")
 	println(part1(input))
